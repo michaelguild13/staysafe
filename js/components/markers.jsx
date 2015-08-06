ae877b5849108ff935a2198f5aba7126e7ca4757
stephen guild <Michael@stephens-MBP.home>
2015-08-05 20:41:24 -0400

    added sort to filters. NOTE: Need to fix dup data on map

--- js/components/markers.jsx
+++ js/components/markers.jsx
@@ -26,19 +26,26 @@ var MarkerCollection = React.createClass({
 
     handleFilter: function (evt) {
         var filter = evt.target.dataset.filter,
-            data = this.state.crimes.sort(
-                // function (a,b) {
-                //     debugger;
-                // if (a > b) {
-                //     return -1;
-                // }
-                // if (a < b) {
-                //     return 1;
-                // }
-                //     return 0;
-                // }
-            );
-        this.setState({crimes: data});
+            data = this.state.crimes;
+        
+        var compare = function (filter) {
+            return function (a,b) {
+                var a = a[filter],
+                    b = b[filter];
+                
+                if (a > b) {
+                    return -1;
+                }else if (a < b) {
+                    return 1;
+                } else {
+                    return 0;
+                }
+            };
+        };
+
+        filter = compare(filter); //set filter
+
+        this.setState({crimes: data.sort(filter)}); // sort and update
     },
 
     render: function () {
@@ -46,23 +53,25 @@ var MarkerCollection = React.createClass({
             markers = this.state.crimes || [];
 
         return (
-            <section id="crimes-listings" className="col-sm-4 col-sm-pull-8 panel panel-default">
+            <div className="col-sm-4 col-sm-pull-8 panel panel-default">
                 <div className="row">
-                <a className="col-sm-6" onClick={self.handleFilter} data-filter="event_clearance_group" >Filter on Group</a>
-                <a className="col-sm-6" onClick={self.handleFilter} data-filter="event_clearance_code" >Filter code</a>
+                        <a className="col-sm-6" onClick={self.handleFilter} data-filter="event_clearance_group" >Filter on Group</a>
+                        <a className="col-sm-6" onClick={self.handleFilter} data-filter="event_clearance_code" >Filter code</a>
                 </div>
-                <ul className="list-group">
-                    {markers.map(function(markers){
-                        makeMarker(markers);
-                        return  <li className="list-group-item" key={markers.cad_event_number}>
-                                    <a className="small" onClick={self.handleClick} data-lng={markers.longitude} data-lat={markers.latitude}>
-                                        {markers.event_clearance_group}
-                                    </a>
-                                    <span className="badge">{markers.event_clearance_code}</span>
-                                </li>
-                    })}
-                </ul>
-            </section>
+                <section id="crimes-listings">
+                    <ul className="list-group">
+                        {markers.map(function(markers){
+                            makeMarker(markers);
+                            return  <li className="list-group-item" key={markers.cad_event_number}>
+                                        <a className="small" onClick={self.handleClick} data-lng={markers.longitude} data-lat={markers.latitude}>
+                                            {markers.event_clearance_group}
+                                        </a>
+                                        <span className="badge">{markers.event_clearance_code}</span>
+                                    </li>
+                        })}
+                    </ul>
+                </section>
+            </div>
           );
     }
 });
